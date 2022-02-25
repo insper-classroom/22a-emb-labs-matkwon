a6bb9ad35281d29870e30060b91b762fce8a804d
Matheus Kwon <62735876+matkwon@users.noreply.github.com>
2022-02-25 08:35:05 -0300

    Lab 1

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,16 +23,35 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           PIOC
-#define LED_PIO_ID        ID_PIOC
-#define LED_PIO_IDX       8
-#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+#define LED1_PIO          PIOA
+#define LED1_PIO_ID       ID_PIOA
+#define LED1_PIO_IDX      0
+#define LED1_PIO_IDX_MASK (1 << LED1_PIO_IDX)
 
-// Configuracoes do botao
-#define BUT_PIO			  PIOA
-#define BUT_PIO_ID		  ID_PIOA
-#define BUT_PIO_IDX		  11
-#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+#define LED2_PIO          PIOC
+#define LED2_PIO_ID       ID_PIOC
+#define LED2_PIO_IDX      30
+#define LED2_PIO_IDX_MASK (1 << LED2_PIO_IDX)
+
+#define LED3_PIO          PIOB
+#define LED3_PIO_ID       ID_PIOB
+#define LED3_PIO_IDX      2
+#define LED3_PIO_IDX_MASK (1 << LED3_PIO_IDX)
+
+#define BUT1_PIO		  PIOD
+#define BUT1_PIO_ID		  ID_PIOD
+#define BUT1_PIO_IDX	  28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO		  PIOC
+#define BUT2_PIO_ID		  ID_PIOC
+#define BUT2_PIO_IDX	  31
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO		  PIOA
+#define BUT3_PIO_ID		  ID_PIOA
+#define BUT3_PIO_IDX	  19
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
 
 
 /************************************************************************/
@@ -69,17 +88,27 @@ void init(void) {
 	
 	// Ativa o PIO na qual o LED foi conectado
 	// para que possamos controlar o LED.
-	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
 	
 	//Inicializa PC8 como saída
-	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
 	
 	// Inicializa PIO do botao
-	pmc_enable_periph_clk(BUT_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
 	
-	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(LED1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(LED2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(LED3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
 	
-	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 0);
+	pio_pull_up(LED1_PIO, BUT1_PIO_IDX_MASK, PIO_PULLUP);
+	pio_pull_up(LED2_PIO, BUT2_PIO_IDX_MASK, PIO_PULLUP);
+	pio_pull_up(LED3_PIO, BUT3_PIO_IDX_MASK, PIO_PULLUP);
 
 }
 
@@ -97,15 +126,36 @@ int main(void) {
 	// aplicacoes embarcadas não devem sair do while(1).
 	while (1)
 	{
-		pio_set(LED_PIO, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
-		delay_ms(5);                        // Delay por software de 10 ms
-		if(!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+		delay_ms(10);                        // Delay por software de 10 ms
+		if(!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)) {
 			for (int i = 0; i < 5; i++) {
-				pio_clear(LED_PIO, LED_PIO_IDX_MASK);
+				pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
 				delay_ms(200);
-				pio_set(LED_PIO, LED_PIO_IDX_MASK);
+				pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
 				delay_ms(200);
 			}
+		} else {
+			pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+		}
+		if(!pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK)) {
+			for (int i = 0; i < 5; i++) {
+				pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+				delay_ms(200);
+				pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+				delay_ms(200);
+			}
+		} else {
+			pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+		}
+		if(!pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK)) {
+			for (int i = 0; i < 5; i++) {
+				pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+				delay_ms(200);
+				pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+				delay_ms(200);
+			}
+		} else {
+			pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
 		}
 	}
 	return 0;
