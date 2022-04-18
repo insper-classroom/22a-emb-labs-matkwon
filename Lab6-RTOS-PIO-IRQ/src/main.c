8a356a87d25477d05375bdeffb319e3431052577
Matheus Kwon <62735876+matkwon@users.noreply.github.com>
2022-04-18 15:23:36 -0300

    update

--- Lab6-RTOS-PIO-IRQ/src/main.c
+++ Lab6-RTOS-PIO-IRQ/src/main.c
@@ -203,18 +203,18 @@ static void task_but(void *pvParameters) {
  * \brief Configure the console UART.
  */
 static void configure_console(void) {
-  const usart_serial_options_t uart_serial_options = {
-      .baudrate = CONF_UART_BAUDRATE,
-      .charlength = CONF_UART_CHAR_LENGTH,
-      .paritytype = CONF_UART_PARITY,
-      .stopbits = CONF_UART_STOP_BITS,
-  };
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = CONF_UART_BAUDRATE,
+		.charlength = CONF_UART_CHAR_LENGTH,
+		.paritytype = CONF_UART_PARITY,
+		.stopbits = CONF_UART_STOP_BITS,
+	};
 
-  /* Configure console UART. */
-  stdio_serial_init(CONF_UART, &uart_serial_options);
+	/* Configure console UART. */
+	stdio_serial_init(CONF_UART, &uart_serial_options);
 
-  /* Specify that stdout should not be buffered. */
-  setbuf(stdout, NULL);
+	/* Specify that stdout should not be buffered. */
+	setbuf(stdout, NULL);
 }
 
 void pin_toggle(Pio *pio, uint32_t mask) {
